c422c76bb20819116cca26e91dcf5316ec9f4dc6
wydra76yan <yan@vydra.com>
2018-03-05 13:42:51 +0300

    Initial commit

---
+++ js/ForMain.js
@@ -0,0 +1,25 @@
+let i = 0;
+let images = [];
+let time = 5000;
+let slide = document.getElementById('slide');
+
+images[0] = "images/FirstSliderImage.jpg";
+images[1] = "images/SecondSliderImage.jpg";
+images[2] = "images/ThirdSliderImage.jpg";
+
+function changeImg(){
+    //slide.classList.toggle('fadeIn');
+    document.getElementById('slide').src = images[i];
+    if(i < images.length - 1){
+        i++;
+    } else {
+        i = 0;
+    }
+    //setTimeout('slide.classList.toggle(\'fadeOut\')',2000);
+    slide.classList.toggle('fadeInLeft');
+    setTimeout("changeImg()", time);
+
+
+}
+//slide.classList.toggle('fadeInLeft');
+window.onload=changeImg;
